88f44390c0f4f64b45abbce3c115e2a724b72f98
Sabfes <bitaev.db@gmail.com>
2023-01-19 23:36:48 +0300

    added github - actions yml file

---
+++ src/components/App/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <section className={'sidebar'}>
+
+      </section>
+      <main className={'layout'}>
+          <p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p><p>qasd</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
