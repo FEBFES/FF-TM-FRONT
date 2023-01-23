78d51780b18f5fcd9388d16fa1227c22fe1ee839
Sabfes <bitaev.db@gmail.com>
2023-01-23 12:08:57 +0300

    added first project stucture

---
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -0,0 +1,31 @@
+import React, {useEffect} from "react";
+import MainLayoutSidebar from "./MainLayoutSidebar";
+import "./MainLayout.scss"
+
+type MainLayoutProps = {
+    children: React.ReactNode;
+    pageTitle: string;
+}
+
+const MainLayout: React.FC<MainLayoutProps> = ({children, pageTitle}): JSX.Element => {
+    useEffect(() => {
+        let initialDocTitle = document.title
+
+        return () => {
+            document.title = initialDocTitle
+        }
+    })
+
+    useEffect(() => {
+        document.title = pageTitle
+    }, [pageTitle])
+
+
+    return  <div className={'mainLay'}>
+        <MainLayoutSidebar/>
+        <div className={'page'}>
+            {children}
+        </div>
+    </div>
+}
+export default MainLayout
---
+++ src/pages/KanbanPage/KanbanPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const KanbanPage: React.FC = (): JSX.Element => {
+    return <div>
+        KanbanPage
+    </div>
+}
+export default KanbanPage;
