9d36ea975385fe7895d7ba8633056284c259e984
Sabfes <bitaev.db@gmail.com>
2023-02-04 23:04:47 +0300

    fix: main layout sidebar

--- src/layouts/MainLayout/MainLayoutSidebar.tsx
+++ src/layouts/MainLayout/MainLayoutSidebar.tsx
@@ -3,41 +3,69 @@ import './MainLayoutSidebar.scss';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faF,
   faCalendar,
-  faUser,
+  faFolder,
+  faCircleExclamation,
+  faCog,
+  faBell,
+  faRightFromBracket,
   faComments,
   faTableColumns,
 } from '@fortawesome/free-solid-svg-icons';
+import { LogoIcon } from '../../assets/icons/LogoIcon';
 
 export const MainLayoutSidebar: React.FC = (): JSX.Element => {
   return (
     <div className={'sidebar'}>
-      <div className={'d-flex'}>
-        <FontAwesomeIcon size={'lg'} icon={faF} />
+      <div className={'sidebar__header'}>
+        <LogoIcon />
       </div>
-      <ul>
-        <li>
-          <Link to={'/'}>
-            <FontAwesomeIcon size={'sm'} icon={faTableColumns} />
-          </Link>
-        </li>
-        <li>
-          <Link to={'/'}>
-            <FontAwesomeIcon size={'sm'} icon={faCalendar} />
-          </Link>
-        </li>
-        <li>
-          <Link to={'/'}>
-            <FontAwesomeIcon size={'sm'} icon={faComments} />
-          </Link>
-        </li>
+
+      <ul className={'sidebar__main'}>
+        <p className={'sidebar__title'}>Overview</p>
+        <Link className={'link'} to={'/'}>
+          <FontAwesomeIcon size={'lg'} icon={faTableColumns} />
+          <span>Dashboard</span>
+        </Link>
+
+        <Link className={'link active'} to={'/'}>
+          <FontAwesomeIcon size={'lg'} icon={faFolder} />
+          <span>Projects</span>
+        </Link>
+
+        <Link className={'link'} to={'/'}>
+          <FontAwesomeIcon size={'lg'} icon={faCalendar} />
+          <span>Calendar</span>
+        </Link>
+
+        <Link className={'link'} to={'/'}>
+          <FontAwesomeIcon size={'lg'} icon={faComments} />
+          <span>Message</span>
+        </Link>
+
+        <Link className={'link'} to={'/'}>
+          <FontAwesomeIcon size={'lg'} icon={faBell} />
+          <span>Notifications</span>
+        </Link>
       </ul>
 
-      <footer>
-        <div className={'footer__avatar'}>
-          <FontAwesomeIcon size={'lg'} icon={faUser} />
-        </div>
+      <footer className={'sidebar__footer'}>
+        <p className={'sidebar__title'}>General</p>
+
+        <Link className={'link'} to={'/'}>
+          <FontAwesomeIcon size={'lg'} icon={faCog} />
+          <span>Settings</span>
+        </Link>
+
+        <Link className={'link'} to={'/'}>
+          <FontAwesomeIcon size={'lg'} icon={faCircleExclamation} />
+          <span>About</span>
+        </Link>
+
+        <Link className={'link'} to={'/'}>
+          <FontAwesomeIcon size={'lg'} icon={faRightFromBracket} />
+          <span>Logout</span>
+        </Link>
       </footer>
     </div>
   );
