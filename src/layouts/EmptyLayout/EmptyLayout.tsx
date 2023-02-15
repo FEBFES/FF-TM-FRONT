73bdc65da1744e3413c0f9ffa5d7c078f208bd03
Sabfes <bitaev.db@gmail.com>
2023-02-15 12:30:29 +0300

    feature: added 404 page

--- src/layouts/EmptyLayout/EmptyLayout.tsx
+++ src/layouts/EmptyLayout/EmptyLayout.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect } from 'react';
 import styles from './EmptyLayout.module.css';
+import { LogoIconDark } from '../../assets/icons/LogoIconDark';
+import { LogoIconLight } from '../../assets/icons/LogoIconLight';
+import { useTypedSelector } from '../../hooks/redux';
 
 interface EmptyLayoutProps {
   children: React.ReactNode;
@@ -10,6 +13,8 @@ export const EmptyLayout: React.FC<EmptyLayoutProps> = ({
   children,
   pageTitle,
 }): JSX.Element => {
+  const theme = useTypedSelector((state) => state.app.theme);
+
   //todo объеединить в хук во всех лайоутах
   useEffect(() => {
     const initialDocTitle = document.title;
@@ -23,5 +28,12 @@ export const EmptyLayout: React.FC<EmptyLayoutProps> = ({
     document.title = pageTitle;
   }, [pageTitle]);
 
-  return <div className={styles.emptyLayout}>{children}</div>;
+  return (
+    <div className={styles.emptyLayout}>
+      <div className={styles.logo}>
+        {theme === 'dark' ? <LogoIconDark /> : <LogoIconLight />}
+      </div>
+      {children}
+    </div>
+  );
 };
