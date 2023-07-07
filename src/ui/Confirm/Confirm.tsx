f52c4778b3c5c9a9bae013d82423e20045044a91
Sabfes <bitaev.db@gmail.com>
2023-07-07 15:31:21 +0300

    feat: added confirm modal to UI

---
+++ src/ui/Confirm/Confirm.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './Confirm.module.css';
+import { Modal } from '../Modal/Modal';
+import { ConfirmProps } from './Confirm.props';
+import { Button } from '../Button/Button';
+import i18n from 'i18next';
+
+export const Confirm: React.FC<ConfirmProps> = ({
+  show,
+  setShow,
+  title,
+  onConfirm,
+}): JSX.Element => {
+  return (
+    <Modal show={show} setShow={setShow}>
+      <div className={styles.cont}>
+        <h1 className={styles.title}>{title}</h1>
+        <div className={styles.btnCont}>
+          <Button theme={'danger'} onClick={() => setShow(false)}>
+            {i18n.t('utils.buttons.cancel')}
+          </Button>
+          <Button
+            theme={'primary'}
+            onClick={() => {
+              onConfirm();
+              setShow(false);
+            }}
+          >
+            {i18n.t('utils.buttons.confirm')}
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  );
+};
