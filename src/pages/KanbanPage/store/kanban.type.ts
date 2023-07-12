5a4442ce9cd9b06473ed73cd9fe2098f4663bcfe
Sabfes <bitaev.db@gmail.com>
2023-07-12 11:02:25 +0300

    feat: added members to project

---
+++ src/pages/KanbanPage/store/kanban.type.ts
@@ -0,0 +1,9 @@
+export interface IMember {
+  displayName: string | null;
+  email: string | null;
+  firstName: string | null;
+  id: number | null;
+  lastName: string | null;
+  userPic: string | null;
+  username: string | null;
+}
