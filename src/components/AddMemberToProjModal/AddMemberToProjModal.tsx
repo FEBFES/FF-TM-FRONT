f1ac9978b0b1b895f4bed79dfb70dc492b187e3f
Sabfes <bitaev.db@gmail.com>
2023-08-23 21:02:11 +0300

    Added member modal

---
+++ src/components/AddMemberToProjModal/AddMemberToProjModal.tsx
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from 'react';
+import styles from './AddMemberToProjModal.module.css';
+import { Modal } from '../../ui/Modal/Modal';
+import { useAppDispatch, useTypedSelector } from '../../hooks/redux';
+import { IMember } from '../../pages/KanbanPage/store/kanban.type';
+import { MemberCard } from '../../pages/KanbanPage/components/MemberCard/MemberCard';
+import { instance } from '../../api/http';
+import { fetchAddMemberToProject } from '../../pages/KanbanPage/store/kanban.thunk';
+import { Title } from '../../ui/Typography';
+import { CloseIcon } from '../../assets/icons/UtilsIcons';
+import { InputField } from '../../ui/InputField/InputField';
+import { Button } from '../../ui/Button/Button';
+import { AvatarGroup } from '../../ui/AvatarGroup/AvatarGroup';
+
+interface AddMemberToProjModalProps {
+  show: boolean;
+  setShow: (bool: boolean) => void;
+}
+
+export const AddMemberToProjModal: React.FC<AddMemberToProjModalProps> = ({
+  show,
+  setShow,
+}): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const projId = useTypedSelector((state) => state.curProj.projId);
+  const [users, setUsers] = useState<IMember[] | []>([]);
+  const [inputValue, setInputValue] = useState<string>('');
+  const [selectedUsers, setSelectedUsers] = useState<IMember[]>([]);
+
+  //todo change all
+  useEffect(() => {
+    instance.get('/users').then((res) => {
+      if (res.status === 200) {
+        setUsers(res.data);
+      }
+    });
+  }, []);
+
+  const addMemberToProj = (memberIds: IMember[]) => {
+    // @ts-ignore
+    const ids: number[] = memberIds.map((el) => el.id);
+    projId &&
+      dispatch(
+        fetchAddMemberToProject({ projId: projId, memberIds: ids })
+      ).finally(() => {
+        setShow(false);
+      });
+  };
+
+  const selectUserHandler = (member: IMember) => {
+    const isMemberInArr = selectedUsers.find((el) => el.id === member.id);
+    !isMemberInArr && setSelectedUsers([...selectedUsers, member]);
+  };
+
+  return (
+    <Modal show={show} setShow={setShow}>
+      <div className={styles.modal}>
+        <div className={styles.modal__header}>
+          <Title level={'h4'}>Добавить участника</Title>
+
+          <div onClick={() => setShow(false)}>
+            <CloseIcon />
+          </div>
+        </div>
+
+        <div className={styles.modal__search}>
+          <InputField
+            withLabel
+            placeholder={'Введите логин или email'}
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+        </div>
+        <div className={`${styles.modal__users} scrollbar`}>
+          {users.length ? (
+            users.map((member: IMember) => {
+              return (
+                <MemberCard
+                  member={member}
+                  onClick={() => selectUserHandler(member)}
+                />
+              );
+            })
+          ) : (
+            <div>Пользователи не найдены...</div>
+          )}
+        </div>
+
+        {selectedUsers.length !== 0 && (
+          <div className={styles.modal__selectedUsers}>
+            <AvatarGroup members={selectedUsers} avatarSize={'m'} />
+          </div>
+        )}
+
+        <div className={styles.modal__footer}>
+          <Button
+            disabled={selectedUsers.length === 0}
+            variant={'secondary'}
+            onClick={() => setSelectedUsers([])}
+          >
+            Отмена
+          </Button>
+          <Button
+            disabled={selectedUsers.length === 0}
+            onClick={() => addMemberToProj(selectedUsers)}
+          >
+            Добавить
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  );
+};
