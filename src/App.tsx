1dfd6bb58685699d9a89af57f52a4f4fe1b4776f
Denis Bitaev <bitaev.db@gmail.com>
2026-05-15 01:23:34 +0300

    asdasd

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <h1>Vite + React</h1>
-      <h1>Vite + React</h1>
-      <h1>Vite + React</h1>
-      <h1>Vite + React</h1>
-      <h1>Vite + React</h1>
-      <h1>Vite + React</h1>
+    <div>
       <h1>Vite + React</h1>
 
       <div>
@@ -57,7 +50,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
 
